6ed3ea3d790fcb1392e1039676d83ef9310ea60c
kjbowman00 <kjbowman00@vt.edu>
2020-03-28 16:13:12 -0400

    One box renders now

--- js/scripts.js
+++ js/scripts.js
@@ -4,20 +4,28 @@ canvas.height = window.innerHeight;
 
 var deltaTime = 0; //In milliseconds
 
+var xPrevious = 0;
+var yPrevious = 0;
+var xPR = 0;
+var yPR = 0;
 var xP = 0;
 var yP = 0;
+var velocity = 3;
 
 function update() {
-	xP += deltaTime*xDir;
-	yP += deltaTime*yDir;
+	xPrevious = xP;
+	yPrevious = yP;
+	xP += deltaTime*xDir*velocity;
+	yP += deltaTime*yDir*velocity;
 }
 
 function draw() {
 	var ctx = canvas.getContext("2d");
 	//Round xP and yP
-	var rxP = (0.5 + xP) | 0;
-	var ryP = (0.5 + yP) | 0;
-	ctx.fillRect(rxP, ryP, 50, 50);
+	ctx.clearRect(0, 0, canvas.width, canvas.height);
+	xPR = ((0.5 + xP) | 0);
+	yPR = ((0.5 + yP) | 0);
+	ctx.fillRect(xPR, yPR, 50, 50);
 }
 
 function mainLoop(timestamp) {

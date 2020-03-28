a6454da8437bb9b12702fed6014642ce257a5155
kjbowman00 <kjbowman00@vt.edu>
2020-03-28 16:50:11 -0400

    world coordinates

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,8 @@
 var canvas = document.getElementById("canvas");
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
+camera.w = canvas.width;
+camera.h = canvas.height;
 
 var deltaTime = 0; //In milliseconds
 
@@ -12,6 +14,14 @@ var xP = 0;
 var yP = 0;
 var velocity = 3;
 
+function inBounds(inner, area) {
+	if (inner.x + inner.w >= area.x && inner.x <= area.x + area.w) {
+		if (inner.y + inner.h >= area.y && inner.y <= area.y + area.h) {
+			return true;
+		} else return false;
+	} else return false;
+}
+
 function update() {
 	xPrevious = xP;
 	yPrevious = yP;
@@ -21,8 +31,18 @@ function update() {
 
 function draw() {
 	var ctx = canvas.getContext("2d");
-	//Round xP and yP
+	//Clear canvas
 	ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+	//Draw world objects where the camera is
+	for (var i in world.things) {
+		item = world.things[i];
+		if (inBounds(item, camera)) {
+			ctx.fillRect(item.x - camera.x, item.y - camera.y, item.w, item.h);
+		}
+	}
+
+	//Round xP and yP
 	xPR = ((0.5 + xP) | 0);
 	yPR = ((0.5 + yP) | 0);
 	ctx.fillRect(xPR, yPR, 50, 50);

c6581b9df11bfe5470366ab65bc9ea96cda1fee4
kjbowman00 <kjbowman00@vt.edu>
2020-04-03 01:56:36 -0400

    Nginx config to serve static content and index

--- public/js/communicator.js
+++ public/js/communicator.js
@@ -1,23 +1,7 @@
-var mainSocket = io();
-var socket;
+var socket = io();
 
-mainSocket.on('join_room_success', function(data) {
-	console.log('Joined room: ' + data);
-
-	socket = io('/G_' + data.toString());
-
-	socket.on('test1', function(data) {
-		console.log('This should run when server msg');
-		console.log('server msg: ' + data);
-	});
-
-	socket.emit('data', 'hehe big boy test!!!');
-
-});
-mainSocket.on('test1', function(data) {
-	console.log('THIS SHOULD NEVER RUN');
+socket.on('test1', function(data) {
+	console.log('Test1 receieved');
 });
 
-
-//Test
-mainSocket.emit('play_game', 'I WANNA PLAY');
+socket.emit('play_game', 'I WANNA PLAY');

86d8d215eac6c121023378496310ac2c8bbc897e
kjbowman00 <kjbowman00@vt.edu>
2020-04-14 19:48:31 -0400

    Server selection websocket working

--- node/gameRoom.js
+++ node/gameRoom.js
@@ -1,10 +1,17 @@
 /*jshint esversion: 6 */
+const gameNum = 1;
+const portNum = 2999 + gameNum;
+
 var express = require('express');
 var app = express();
 var http = require('http').createServer();
+
+var ioPath = '/game' + gameNum + '/socket.io';
 const io = require('socket.io')(http, {
-  path: '/game1/socket.io'
+  path: ioPath
 });
+
+
 
 io.on('connection', function(socket) {
     console.log("websocket established");
@@ -15,6 +22,6 @@ io.on('connection', function(socket) {
     socket.emit('test1', "heyyy");
 });
 
-http.listen(3000, function() {
+http.listen(portNum, function() {
     console.log('listening on localhost:3000');
 });
--- public/js/communicator.js
+++ public/js/communicator.js
@@ -1,11 +1,26 @@
-const socket = io('/', {
-  secure: true,
-  rejectUnauthorized: false,
-  path: '/game1/socket.io'
-});
+/*jshint esversion: 6 */
+function onPlay() {
+	var form = document.getElementById("name_form");
+	var formData = new FormData(form);
+	socketStuff(formData);
+	return false;
+}
 
-socket.on('test1', function(data) {
-	console.log('Test1 receieved');
-});
+document.getElementById("name_form").onsubmit = onPlay;
 
-socket.emit('play_game', 'I WANNA PLAY');
+function socketStuff(formData) {
+	var gameName = formData.get('server');
+	var path = '/' + gameName + '/socket.io';
+    const socket = io('/', {
+        secure: true,
+        rejectUnauthorized: false,
+        path: path
+    });
+
+    socket.on('test1', function(data) {
+        console.log('Test1 receieved');
+    });
+
+    socket.emit('play_game', formData.get('username'));
+
+}

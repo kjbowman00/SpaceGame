64905bc9aefddb0af63b5ee2c59ac957fcea658d
Kyle <kjbowman00@vt.edu>
2020-05-09 22:08:27 -0400

    More collision stuff

--- node/collisionDetection.js
+++ node/collisionDetection.js
@@ -4,15 +4,37 @@ var staticObjsTree = tree2d.makeTreeFromWorld(staticWorldObjs);
 
 function handleStaticObjsCollision(players) {
 	players.forEach((player) => {
-		let bounds = player.bounds;
-		let b1 = tree2d.getClosest(bounds.p1, staticObjsTree);
-		let b2 = tree2d.getClosest(bounds.p2, staticObjsTree);
-		let b3 = tree2d.getClosest(bounds.p3, staticObjsTree);
-		let b4 = tree2d.getClosest(bounds.p4, staticObjsTree);
+		let bounds = { x: player.x, y: player.y, w: player.w, h: player.h };
+
+		//Check the collision with the following four boxes
+		let b1 = tree2d.getClosest({ x:bounds.x, y:bounds.y }, staticObjsTree);
+		let b2 = tree2d.getClosest({ x: bounds.x + bounds.w, y: bounds.y }, staticObjsTree);
+		let b3 = tree2d.getClosest({ x: bounds.x, y: bounds.y + bounds.h }, staticObjsTree);
+		let b4 = tree2d.getClosest({ x: bounds.x + bounds.w, y: bounds.y + bounds.h }, staticObjsTree);
 		let boxesToCheck = [b1, b2, b3, b4];
 		for (let i = 0; i < 4; i++) {
 			let currentBox = boxesToCheck[i];
 			//Check for collision with this box
+			if (doesCollisde(bounds, currentBox)) {
+				//Get the bounds and move the player
+			}
 		}
 	});
+}
+
+function doesCollide(rect1, rect2) {
+	if (rect1.x < rect2.x + rect2.w &&
+		rect1.x + rect1.w > rect2.x &&
+		rect1.y < rect2.y + rect2.h &&
+		rect1.y + rect1.h > rect2.y) {
+		//Collision!
+		console.log("YEET");
+		return true;
+	}
+	return false;
+}
+
+
+function updateCollisions(players) {
+	handleStaticObjsCollision(players);
 }
